46075791e917115aea6c21503d3a2a36a120810f
Mark Acosta <markaacosta@gmail.com>
2023-10-19 15:58:56 -0600

    added nvm completions to bash (app customizations) and zsh (rc)

--- dot_bash/bash-app-customizations.sh
+++ dot_bash/bash-app-customizations.sh
@@ -148,7 +148,11 @@ eval $(thefuck --alias)
 PATH_TO_NPM_COMPLETION="/usr/local/bin/../lib/node_modules/npm-completion"
 source $PATH_TO_NPM_COMPLETION/npm-completion.sh
 
-# enables nvm
+# enables and bootstraps nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# added for npm-completion https://github.com/Jephuff/npm-bash-completion
+PATH_TO_NPM_COMPLETION="/home/mara/.nvm/versions/node/v21.0.0/lib/node_modules/npm-completion"
+source $PATH_TO_NPM_COMPLETION/npm-completion.sh
