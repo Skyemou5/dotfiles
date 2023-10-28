dd4323f49be45399376ef15db9bbd57329ad6a39
Mark Acosta <markaacosta@gmail.com>
2023-10-28 16:44:39 -0600

    updated alias for edit AND apply chez-changes

--- dot_bash/bash-aliases.sh
+++ dot_bash/bash-aliases.sh
@@ -32,4 +32,4 @@ alias skag="sk --ansi -i -c 'ag --color \"{}\"' | sd \":.*\" \"\""
 alias skrg="sk --ansi -i -c 'rg --color=always --line-number \"{}\"' | sd \":.*\" \"\""
 alias edit="$EDITOR"
 alias ncdu="ncdu --show-itemcount"
-alias chezedit='chezmoi edit --apply'
+alias chezwrite='chezmoi edit --apply'
