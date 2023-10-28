68b3c6924c8efb3ec8f6a126c811b6cf26af9636
Mark Acosta <markaacosta@gmail.com>
2023-10-28 07:17:54 -0600

    added asci-adjacent functions for project grok

--- dot_bash/bash-functions.sh
+++ dot_bash/bash-functions.sh
@@ -37,8 +37,22 @@ function list-cargo-global() {
 	cargo install --list | rg -v "^\w" | sd " " "" | sort -u
 }
 
+function ascirec() {
+  command asciinema rec --overwrite "$1"
+}
+
 function gifasciinema() {
   name=$(echo "$1" | rev | cut -d '.' -f 2- | rev)
-  agg --cols 70 --rows 28 --font-size 32 --speed 1.5 --theme dracula "$1" "${name}.gif" 
+  agg --cols 52 --rows 18 --font-size 26 --speed 1.5 --theme dracula "$1" "${name}.gif" 
   rm "$1"
+}
+
+function count-of-tmsu-tags-by-artist() {
+  for item in $(tmsu values -1 artist); do 
+    echo "$item"; 
+    tmsu files artist="$item" -c; 
+  done \
+      | xargs -n 2 \
+      | sed "s| |,|" \
+      | sort -nrt, -k2
 }
