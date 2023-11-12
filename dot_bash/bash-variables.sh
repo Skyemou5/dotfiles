628191ce409be4adac80a9ebe9b941a2cf1af7f0
Mark Acosta <markaacosta@gmail.com>
2023-11-12 10:46:37 -0700

    fixed conundrum between how chezmoi handles EDITOR determination, unwanted alias expansion for 'edit' within the broader 'chezwrite' alias, etc. the fix present in "bash-aliases" file allowed removing the config option from "chezmoi.toml"

--- dot_bash/bash-variables.sh
+++ dot_bash/bash-variables.sh
@@ -1,6 +1,20 @@
 #environment variables
 PAGER='/usr/bin/less'
+
 #EDITOR='nano -lP'
-EDITOR='nvim'
+
+#find best editor available
+if command -v lvim &> /dev/null; then
+  VISUAL='lvim'
+elif command -v nvim &> /dev/null; then
+  VISUAL='nvim'
+elif command -v vim &> /dev/null; then
+  VISUAL='vim'
+else
+  VISUAL='nano'
+fi
+
+EDITOR="$VISUAL"
+
 #TERMINAL for i3-sensible-terminal
 TERMINAL='/usr/bin/kitty'
