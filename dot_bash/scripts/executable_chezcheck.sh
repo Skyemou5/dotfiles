ca5a8c2b6e9e4410f8707de1761adb07ee31df99
Mark Acosta <markaacosta@gmail.com>
2023-11-11 21:01:06 -0700

    new rules for opening i3 windows
    wrote script to keep track of chezmoi repo status locally and in the
    cloud

--- dot_bash/scripts/executable_chezcheck.sh
+++ dot_bash/scripts/executable_chezcheck.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [[ ! $(chezmoi git diff) == "" ]]; then
-  echo "!="
+# enter chezmoi repo to run git commands
+cd ~/.local/share/chezmoi
+
+# ensure local is committed and upstream is identical to local
+# in essence; ensure all changes are committed, but also PUSHED
+if [[ -z $(git status --porcelain) && -z $(git diff @{u}) ]]; then
+  echo "☑️"
 else
-  echo "="
+  echo "🔲"
 fi
